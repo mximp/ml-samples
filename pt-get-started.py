baaa9879f27d64b4328e01f23f8857e84fface43
mximp <mxim.petrov@gmail.com>
2024-04-30 10:17:41 +0300

    linear regress

---
+++ pt-get-started.py
@@ -0,0 +1,48 @@
+import torch
+import matplotlib.pyplot as plt
+
+X = torch.arange(-5, 5, 0.1).view(-1, 1)
+func = -5 * X
+Y = func + 0.4 * torch.randn(X.size())
+
+
+# defining the function for forward pass for prediction
+def forward(x):
+    return w * x
+
+
+# evaluating data points with Mean Square Error
+def criterion(y_pred, y):
+    return torch.mean((y_pred - y) ** 2)
+
+
+w = torch.tensor(-10.0, requires_grad=True)
+
+step_size = 0.1
+loss_list = []
+cycles = 20
+
+for i in range(cycles):
+    # making predictions with forward pass
+    Y_pred = forward(X)
+    # calculating the loss between original and predicted data points
+    loss = criterion(Y_pred, Y)
+    # storing the calculated loss in a list
+    loss_list.append(loss.item())
+    # backward pass for computing the gradients of the loss w.r.t to
+    # learnable parameters
+    loss.backward()
+    # updating the parameters after each iteration
+    w.data = w.data - step_size * w.grad.data
+    # zeroing gradients after each iteration
+    w.grad.data.zero_()
+    # printing the values for understanding
+    print('{},\t{},\t{}'.format(i, loss.item(), w.item()))
+
+# Plotting the loss after each iteration
+plt.plot(loss_list, 'r')
+plt.tight_layout()
+plt.grid(True, color='y')
+plt.xlabel("Epochs/Iterations")
+plt.ylabel("Loss")
+plt.show()
